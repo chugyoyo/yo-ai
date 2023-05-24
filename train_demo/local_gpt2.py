e2692b9310169a4a7743d646202d6248a55cfee3
吴泽欣 <wuzexin@dslyy.com>
2023-05-24 22:45:06 +0800

    :arrow_down: model_name -> gpt2

--- train_demo/local_gpt2.py
+++ train_demo/local_gpt2.py
@@ -14,23 +14,13 @@ GPT2-Large: The GPT2-Large model has approximately 774 million parameters.
 GPT2-XL: The GPT2-XL model has approximately 1.5 billion parameters. 
 The storage size of the GPT2-XL model is approximately 6 GB.
 '''
-# 加载预训练的GPT-2模型和分词器
-# model_name = 'gpt2'  # 使用预训练的GPT-2模型
-model_name = 'GPT2-XL'  # 使用预训练的GPT-2模型
+
+model_name = 'GPT2-2'
 model = GPT2LMHeadModel.from_pretrained(model_name)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 
-# 输入文本
 input_text = "translate the word 'billion' to Chinese"
-
-# 对输入文本进行分词
 input_ids = tokenizer.encode(input_text, return_tensors='pt')
-
-# 使用模型生成文本
 output = model.generate(input_ids, max_length=100, num_return_sequences=1)
-
-# 解码生成的文本
 generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-
-# 输出生成的文本
 print(generated_text)
